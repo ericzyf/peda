3022ea07c2faaf190037c6699356621a484194ac
Yifan Zhang <yfzhang1999@gmail.com>
2023-10-19 15:07:37 +0000

    Change prompt color to purple

--- peda.py
+++ peda.py
@@ -6151,7 +6151,7 @@ Alias("brva", "breakrva")
 peda.execute("set confirm off")
 peda.execute("set verbose off")
 peda.execute("set output-radix 0x10")
-peda.execute("set prompt \001%s\002" % blue("\002peda$ \001", "bold")) # custom prompt
+peda.execute("set prompt \001%s\002" % colorize("\002peda$ \001", "purple", "bold")) # custom prompt
 peda.execute("set height 0") # disable paging
 peda.execute("set history expansion on")
 peda.execute("set history save off") # disable history saving
